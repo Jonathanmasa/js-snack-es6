0900ef82be86143d3470a335fb15280a54cc9092
Jonathanmasa <jonathanmasa97@gmail.com>
2025-01-16 15:50:35 +0100

    feat[snack1]

--- js/script.js
+++ js/script.js
@@ -1,6 +1,55 @@
 // Snack 1
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 
+// creo l'array di oggetti
+const bici = [
+    {
+        'nome': 'atala',
+        'peso': 6,
+    },
+
+    {
+        'nome': 'carrera',
+        'peso': 4,
+    },
+
+    {
+        'nome': 'bianchi',
+        'peso': 8,
+    },
+
+    {
+        'nome': 'ktm',
+        'peso': 10,
+    },
+
+    {
+        'nome': 'focus',
+        'peso': 5,
+    },
+];
+console.table(bici);
+
+// creo variabile peso  minore
+let biciLeggera = bici[0];
+
+// ciclo for per trovare la bici con il peso minore
+for (let i = 1; i < bici.length; i++) {
+
+    if (bici[i].peso < biciLeggera.peso) {
+        biciLeggera = bici[i];
+
+    }
+}
+
+// stampo il risultato
+console.log("la bici con il peso minore è:", biciLeggera);
+
+    
+
+
+
+
 
 
 // Snack2
